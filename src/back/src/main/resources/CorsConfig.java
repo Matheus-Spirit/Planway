12068f295b05d3ebb0aaa2cf01b0310dfceafaf9
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2024-12-14 17:57:18 -0300

    Teste2

---
+++ src/back/src/main/resources/CorsConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todas as rotas
+                        .allowedOrigins("https://planway-one.vercel.app") // Domínio do Vercel
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Headers permitidos
+                        .allowCredentials(true); // Permitir credenciais
+            }
+        };
+    }
+}
+
